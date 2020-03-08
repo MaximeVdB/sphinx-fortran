f126edb15df531009a0b45183bcb2c0bb2c47a4b
MaximeVdB <maxime.cp.vandenbossche@gmail.com>
2020-03-08 15:45:31 -0400

    HACK: sort crashes if multiple types -> remove it

--- sphinxfortran/fortran_autodoc.py
+++ sphinxfortran/fortran_autodoc.py
@@ -1244,7 +1244,7 @@ class F90toRst(object):
 
         # Types
         decs = []
-        tlist = sorted(self.get_blocklist('types', module))
+        tlist = self.get_blocklist('types', module) #sorted(self.get_blocklist('types', module))
         if tlist:
             decs.append(':Types: ' +
                         ', '.join([':f:type:`%s`' %
